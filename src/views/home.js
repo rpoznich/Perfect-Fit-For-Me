15954f8f0f4fda5c16588fe9066bcfd01e054cb5
Ryan Poznich <ryanpoznich@gmail.com>
2019-03-02 00:02:09 -0600

    changed background picture of home page, looks pretty good

--- src/views/home.js
+++ src/views/home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { homedir } from 'os';
-import '../css/home.css'
+import '../css/home.css';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
 class Home extends Component {
     constructor(props)
@@ -35,15 +36,10 @@ class Home extends Component {
             <header class="masthead text-center text-white">
                 <div class="masthead-content">
                 <div class="container">
-                    <h1 class="masthead-heading mb-0">One Page Wonder</h1>
-                    <h2 class="masthead-subheading mb-0">Will Rock Your Socks Off</h2>
-                    <a href="#" class="btn btn-primary btn-xl rounded-pill mt-5">Learn More</a>
+                    <h1 class="masthead-heading mb-0">Find Your Perfect Fit</h1>
+                    <Link className="btn btn-primary btn-xl rounded-pill mt-5" to="/about" onClick={() => this.handleClick("about")}> Learn More</Link>
                 </div>
                 </div>
-                <div class="bg-circle-1 bg-circle"></div>
-                <div class="bg-circle-2 bg-circle"></div>
-                <div class="bg-circle-3 bg-circle"></div>
-                <div class="bg-circle-4 bg-circle"></div>
             </header>
 
             <section>
@@ -102,7 +98,7 @@ class Home extends Component {
 
             <footer class="py-5 bg-black">
                 <div class="container">
-                <p class="m-0 text-center text-white small">Copyright &copy; Your Website 2019</p>
+                <p class="m-0 text-center text-white small">Copyright &copy; perfectfitfor.me 2019</p>
                 </div>
             </footer>
 
