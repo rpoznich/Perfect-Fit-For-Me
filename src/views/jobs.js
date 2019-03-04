7a89dd4ac83e1aa6714f92d6125a1e2dc5a90978
Ozone Kafley <kafleyozone@gmail.com>
2019-03-04 16:08:54 -0600

    jobs page styles updated, fix #10

--- src/views/jobs.js
+++ src/views/jobs.js
@@ -1,8 +1,71 @@
 import React, { Component } from 'react';
 import { homedir } from 'os';
+import '../css/city/style.scss';
+import '../css/city/bootstrap.min.css';
+import '../css/city/simple-line-icons.css';
+import '../css/city/font-awesome.min.css';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+
+class JobListing extends Component{
+    constructor(props){
+        super(props);
+        this.state = {}
+        if(this.props.overall_rating < 40.0){
+            this.state.circleColor = "featured-rating";
+        }else if(this.props.overall_rating >= 70.0){
+            this.state.circleColor = "featured-rating-green";
+        }else{
+            this.state.circleColor = "featured-rating-orange";
+        }
+        this.state.highlightedDS = "";
+        this.state.unhighlightedDS = "";
+        let bound = (Math.ceil(this.props.cost_of_living / 2))
+        for(let i = 0; i < bound; ++i){
+            this.state.highlightedDS += "$";
+        }
+        for(let j = 0; j < 5 - (this.state.highlightedDS.length); ++j){
+            this.state.unhighlightedDS += "$";
+        }
+    }
+
+    render(){
+        return (<div class="featured-place-wrap">
+                            <a href={"/jobs/"+this.props.id}>
+                            {/* <img src={""+ this.props.image} height="200" width="100"></img> */}
+                            {/* <div className="container">
+                                <span className={this.state.circleColor} title="Overall Rating">{this.props.overall_rating / 10.0}</span>
+                            </div> */}
+                            <div class="featured-title-box">
+                                <h6 >{this.props.title}</h6>
+                                {/* <p>Restaurant </p> <span>• </span>
+                                <p>3 Reviews</p> <span> • </span> */}
+                                {/* <p><span>{this.state.highlightedDS}</span>{this.state.unhighlightedDS}</p> */}
+                                <ul>
+                                    <li><span class="icon-location-pin"></span>
+                                        <p>{this.props.location}</p>
+                                    </li>
+                                    <li><span></span>
+                                        <p>{this.props.company}</p>
+                                    </li>
+                                    <li><span></span>
+                                        <p>{this.props.type}</p>
+                                    </li>
+                                    <li><span></span>
+                                        <p>{this.props.time}</p>
+                                    </li>
+
+                                </ul>
+                                {/* <div class="bottom-icons">
+                                    <span class="ti-heart"></span>
+                                    <span class="ti-bookmark"></span>
+                                </div> */}
+                                </div>
+                                </a>
+                    </div>)
+    }
+}
 
 class Jobs extends Component {
-
     constructor(props)
     {
         super(props);
@@ -20,7 +83,7 @@ class Jobs extends Component {
             }
         }
     }
-
+    
     componentDidMount()
     {
         fetch('../statics/data.json').then(response => { //change this to actual API
@@ -28,84 +91,50 @@ class Jobs extends Component {
           }).then(data => {
             // Work with JSON data here
             this.state.jobs = data.jobs
+            this.setState(this.state);
           }).catch(err => {
             // Do something for an error here
             console.log("Error Reading data " + err);
           });
     }
-    
-    
+
     render()
     {
-        if(this.isListing)
+        if(!this.props.isListing){
+        let components = [];
+        for(let id in this.state.jobs)
         {
-            return(
-                <div className="main" style={{marginTop: "20vh"}}>
-                    <div className="container">
-                        <h1>Find Jobs</h1>
-                        <hr />
-                        <div className="input-group col-md-12">
-                            <div className="input-group-prepend">
-                                <span className="input-group-text">Zipcode</span>
-                            </div>
-                            <input type="text" className="form-control"/>
-                            <h4 className="ml-3 mr-3">- or -</h4>
-                            <div className="input-group-prepend">
-                                <span className="input-group-text">City</span>
-                            </div>
-                            <input type="text" className="form-control"/>
-                            <select className="ml-3">
-                                <option>State</option>
-                                <option>TX</option>
-                                <option>CA</option>
-                            </select>
-                        </div>
-                        <button className="btn btn-primary m-3 col-md-1">Search</button>  
-                        <h2 style={{marginTop: 1 + "em"}}>3 results found:</h2> 
-                        <div className="card-deck">
-                            <div className="card">
-                                <img class="card-img-top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22309%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20309%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16937b69c28%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A15pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16937b69c28%22%3E%3Crect%20width%3D%22309%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22116.04999923706055%22%20y%3D%2296.6%22%3E309x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Card image cap"/>
-                                <div class="card-body">
-                                    <a href="#"><h5 class="card-title">Job 1</h5></a>
-                                    <ul>
-                                        <li>Attribute</li>
-                                        <li>Attribute</li>
-                                        <li>Attribute</li>
-                                        <li>Attribute</li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div className="card">
-                                <img class="card-img-top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22309%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20309%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16937b69c28%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A15pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16937b69c28%22%3E%3Crect%20width%3D%22309%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22116.04999923706055%22%20y%3D%2296.6%22%3E309x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Card image cap"/>
-                                <div class="card-body">
-                                    <h5 class="card-title">Job 2</h5>
-                                    <ul>
-                                        <li>Attribute</li>
-                                        <li>Attribute</li>
-                                        <li>Attribute</li>
-                                        <li>Attribute</li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div className="card">
-                                <img class="card-img-top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22309%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20309%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_16937b69c28%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A15pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_16937b69c28%22%3E%3Crect%20width%3D%22309%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22116.04999923706055%22%20y%3D%2296.6%22%3E309x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Card image cap"/>
-                                <div class="card-body">
-                                    <h5 class="card-title">Job 3</h5>
-                                    <ul>
-                                        <li>Attribute</li>
-                                        <li>Attribute</li>
-                                        <li>Attribute</li>
-                                        <li>Attribute</li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
+            components.push( <div className="col-md-4 featured-responsive"><JobListing id={id} {...this.state.jobs[id]}/></div>)
+        }
+        return(
+            <div className="cities">
+            <div className="main" style={{marginTop: "10vh"}}>
+    <section class="main-block light-bg">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-5">
+                    <div class="styled-heading">
+                        <h3>Featured Places</h3>
                     </div>
                 </div>
-            )
-        } else 
-        {
-            return ( <div className="main" style={{marginTop: "20vh"}}> Listing {this.id}</div> )
+            </div>
+            <div class="row">
+                {components}
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-4">
+                    <div class="featured-btn-wrap">
+                        <a href="#" class="btn btn-danger">VIEW ALL</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+             </div>
+             </div>
+        )
+        }else{
+            return (<div></div>);
         }
     }
 }
