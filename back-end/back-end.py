ff3fe159a7c078e6c23ff33334dd02e4751f42f6
Daniel Thai <danielkthai@yahoo.com>
2019-03-27 00:47:39 -0500

    testing scraping methods

--- back-end/back-end.py
+++ back-end/back-end.py
@@ -1,17 +1,9 @@
 from flask import Flask
-import json
-import requests
-import citiesExtract
 app = Flask(__name__)
 
 @app.route('/') 
-def get_default(): 
-	return 'Welcome to the back-end' 
-
-@app.route('/api/cities')
-def get_cities():
-    return citiesExtract.extract_cities()
+def hello_world(): 
+	return 'Hello, World!' 
 
 if __name__ == '__main__': 
-	print("Is this running")
-	app.run(debug=True, host='0.0.0.0')
+	app.run()
