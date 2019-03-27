fa53f86edf048f9a4e11ee2b9f3b15dfa30c9b15
Daniel Thai <danielkthai@yahoo.com>
2019-03-27 00:51:07 -0500

    reverted back-end.py

--- back-end/back-end.py
+++ back-end/back-end.py
@@ -1,9 +1,17 @@
 from flask import Flask
+import json
+import requests
+import citiesExtract
 app = Flask(__name__)
 
 @app.route('/') 
-def hello_world(): 
-	return 'Hello, World!' 
+def get_default(): 
+	return 'Welcome to the back-end' 
+
+@app.route('/api/cities')
+def get_cities():
+    return citiesExtract.extract_cities()
 
 if __name__ == '__main__': 
-	app.run()
+	print("Is this running")
+	app.run(debug=True, host='0.0.0.0')
