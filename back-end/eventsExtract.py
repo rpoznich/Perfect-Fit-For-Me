79480c0fcd3984cc1678e2c06167b94f36e99a94
Daniel Thai <danielkthai@yahoo.com>
2019-03-27 21:05:36 -0500

    added more attributes to events and solved page restriction problem, closes #39

--- back-end/eventsExtract.py
+++ back-end/eventsExtract.py
@@ -23,29 +23,38 @@ def _byteify(data, ignore_dicts = False):
     # if it's anything else, return it in its original form
     return data
 
-def extract_events():
-	# lat/long are center of USA with distance to reach both coasts
-	url = "https://www.eventbriteapi.com/v3/events/search/?sort_by=date&location.within=1500mi&location.latitude=39&location.longitude=-98&token=WG5JAPAZ3A56WMVJKO7K&expand=venue"
-	eventsJSON = requests.get(url).text
-	events = json.loads(eventsJSON, object_hook=_byteify)["events"]
-	return events
-
-if __name__ == "__main__":
+def scrape_events():
 	eventsTable = []
-	events = extract_events() # list of dicts where each dict is an event
-	print(len(events))
-	for i in range(0, 50):
-		eventInstance = {}
-		eventInstance["name"] = events[i]["name"]["text"]
-		eventInstance["summary"] = events[i]["summary"]
-		eventInstance["location"] = events[i]["venue"]["address"]["localized_address_display"]
-		eventInstance["start"] = events[i]["start"]["local"]
-		eventInstance["end"] = events[i]["end"]["local"]
-		eventInstance["timezone"] = events[i]["start"]["timezone"]
-		eventsTable.append(eventInstance)
+	url = "https://www.eventbriteapi.com/v3/events/search/?sort_by=date&location.within=1500mi&location.latitude=39&location.longitude=-98&token=WG5JAPAZ3A56WMVJKO7K&expand=venue"
+
+	for p in range(1, 21): # scrape 20 pages
+		url_with_page = url + "&page=" + str(p)
+		eventsJSON = requests.get(url_with_page).text
+		page_info = json.loads(eventsJSON, object_hook=_byteify)["pagination"]
+		page_size = page_info["page_size"]
+		events = json.loads(eventsJSON, object_hook=_byteify)["events"]
+		for i in range(0, page_size):
+			eventInstance = {}
+			eventInstance["name"] = events[i]["name"]["text"]
+			eventInstance["summary"] = events[i]["summary"]
+			eventInstance["address"] = events[i]["venue"]["address"]["localized_address_display"]
+			eventInstance["venue"] = events[i]["venue"]["name"]
+			eventInstance["start"] = events[i]["start"]["local"]
+			eventInstance["end"] = events[i]["end"]["local"]
+			eventInstance["timezone"] = events[i]["start"]["timezone"]
+			eventInstance["url"] = events[i]["url"]
+			if events[i]["logo"] is not None:
+				eventInstance["logo"] = events[i]["logo"]["url"]
+			eventsTable.append(eventInstance)
+		print("Page " + str(p) + " complete")
 
 	# printing table
 	for e in eventsTable:
 		print(e["name"])
 		for k, v in e.items():
-			print('   ' + k + ': ' + v)
+			if k != "name":
+				print('   ' + str(k) + ': ' + str(v))
+	print(len(eventsTable))
+
+if __name__ == "__main__":
+	scrape_events()
