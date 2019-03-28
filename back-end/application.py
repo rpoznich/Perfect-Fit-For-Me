0db4b6385b060027004d792c75836a6a16422d74
Daniel Thai <danielkthai@yahoo.com>
2019-03-28 18:34:38 -0500

    added cors

--- back-end/application.py
+++ back-end/application.py
@@ -1,32 +1,34 @@
 from flask import Flask
 from flask import jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS, cross_origin
 from models import City, Job, Event, application, db
 import json
 import requests
 
+cors = CORS(application)
+
 #------------------#
 # DATABASE QUERIES #
 #------------------#
-def get_jobs(): 
+def query_jobs(): 
     jobs = {"Jobs":[]}
     job_objs = Job.query.all() 
     for j in job_objs: 
         jobs["Jobs"].append(j.toDict())
     return jobs
 
-def get_one_job_by_id(identifier): 
+def query_jobs_by_id(identifier): 
     j = Job.query.get(identifier)
     return j.toDict()
 
-def get_jobs_by_city(city): 
+def query_jobs_by_city(city): 
     job_objs = Job.query.filter_by(city_name=city).all()
     jobs = {"Jobs":[]}
     for j in job_objs:
         jobs["Jobs"].append(j.toDict())
     return jobs
 
-def get_cities(): 
+def query_cities(): 
     cities = {}
     city_objs = City.query.all()
     for c in city_objs: 
@@ -34,7 +36,7 @@ def get_cities():
         cities[name] = c.toDict()
     return cities
 
-def get_cities_by_state(state):
+def query_cities_by_state(state):
     cities = {}
     city_objs = City.query.filter_by(state=state).all() 
     print(len(city_objs))
@@ -46,30 +48,41 @@ def get_cities_by_state(state):
 #------------#
 # API ROUTES #
 #------------#
-@app.route('/api/events')
+@application.route('/api/events')
+@cross_origin()
 def get_events():
     events = Event.query.all()
     return jsonify([e.json() for e in events])
 
-@app.route('/api/jobs')
+@application.route('/api/jobs')
+@cross_origin()
 def get_jobs(): 
-    return jsonify(get_jobs())
+    return jsonify(query_jobs())
 
-@app.route('/api/jobs/id/<id>')
+@application.route('/api/jobs/id/<id>')
+@cross_origin()
 def get_one_job_by_id(id):
-    return jsonify(get_one_job_by_id(id))
+    return jsonify(query_jobs_by_id(id))
 
-@app.route("/api/jobs/city/<city>")
+@application.route("/api/jobs/city/<city>")
+@cross_origin()
 def get_jobs_by_city(city): 
-    return jsonify(get_jobs_by_city(city))
+    return jsonify(query_jobs_by_city(city))
 
-@app.route('/api/cities')
+@application.route('/api/cities')
+@cross_origin()
 def get_cities():
-    return jsonify(get_cities())
+    return jsonify(query_cities())
 
-@app.route('/api/cities/state/<state>')
+@application.route('/api/cities/state/<state>')
+@cross_origin()
 def get_cities_by_state(state):
-    return jsonify(get_cities_by_state(state))
+    return jsonify(query_cities_by_state(state))
+
+@application.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Credentials', 'true')
+    return response
 
 if __name__ == '__main__': 
 	application.run()
