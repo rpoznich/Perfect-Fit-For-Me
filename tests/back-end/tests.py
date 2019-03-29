6403a9304f012bb07e65814068209e3d035ed9e9
root <dylankile@gmail.com>
2019-03-28 23:21:48 -0500

    Added a total of 9 unittests

--- tests/back-end/tests.py
+++ tests/back-end/tests.py
@@ -79,5 +79,42 @@ class TestModels (TestCase):
         self.assertEqual(event_stats_test, event.json())
 
 
+    #######################
+    # Table Query Testing #
+    #######################
+
+    def test_single_instance_job(self): 
+        job = Job.query.get(1)
+        self.assertEqual(type(job), Job)
+
+    def test_jobs_all_same_city(self): 
+        jobs = Job.query.filter_by(city_name="Austin", state="Texas")
+        all_same = True 
+        for job in jobs: 
+            if job.city_name != "Austin": 
+                all_same = False 
+                break
+        self.assertEqual(all_same, True)
+
+    def test_job_not_in_db(self): 
+        job = Job.query.filter_by(job_title="Homeschool Teacher").all()
+        self.assertEqual(job, [])
+
+    def test_single_instance_city(self): 
+        city = City.query.get(1) 
+        self.assertEqual(type(city), City)
+
+    def test_city_not_in_cb(self): 
+        city = City.query.filter_by(name="Scooby Doo").all() 
+        self.assertEqual(city, []) 
+
+    def test_single_instance_event(self): 
+        event = Event.query.get(1) 
+        self.assertEqual(type(event), Event)
+
+    def test_event_not_in_cb(self): 
+        event = Event.query.filter_by(name="Superhero Slumber Symphony").all() 
+        self.assertEqual(event, [])
+
 if __name__ == '__main__': 
     main()
